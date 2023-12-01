6ba41f0686ffa198ecb1d4a0003f43579b3ae645
YoonJunHyeon <qmdldos@gmail.com>
2023-12-01 15:58:20 +0900

    이동용 저장

--- scriptCording/jsDom.js
+++ scriptCording/jsDom.js
@@ -1,1 +1,23 @@
 console.log('hi');
+const root = document.getElementById('root');
+
+function createTag(tag, container, attr, attrText ) {
+  const tagAdd = document.createElement(tag);
+  container.appendChild(tagAdd).setAttribute(attr, attrText);
+}
+const container = document.createElement('div');
+root.appendChild(container).setAttribute('class','container');
+
+createTag('div',root,'class','container');
+
+const containers = document.getElementsByClassName('container');
+console.dir(containers);
+
+containers[0].id = "top";
+containers[1].id = "bottom";
+
+
+
+
+
+
